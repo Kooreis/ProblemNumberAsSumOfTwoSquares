8e9919d5d0c6fb61505c6ef1a53fb68894fcd905
Kooreis <kooreis.official@gmail.com>
2024-06-10 00:22:33 -0500

    Initial setup of the program. It prompts the user to enter a number.

--- solution.cs
+++ solution.cs
@@ -1,4 +1,3 @@
-```C#
 using System;
 
 class Program
@@ -6,31 +5,5 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("Enter a number: ");
-        int number = Convert.ToInt32(Console.ReadLine());
-
-        if (IsSumOfTwoSquares(number))
-        {
-            Console.WriteLine("The number can be expressed as a sum of two squares.");
-        }
-        else
-        {
-            Console.WriteLine("The number cannot be expressed as a sum of two squares.");
-        }
-    }
-
-    static bool IsSumOfTwoSquares(int number)
-    {
-        for (int i = 0; i * i <= number; i++)
-        {
-            for (int j = 0; j * j <= number; j++)
-            {
-                if (i * i + j * j == number)
-                {
-                    return true;
-                }
-            }
-        }
-        return false;
     }
 }
-```
